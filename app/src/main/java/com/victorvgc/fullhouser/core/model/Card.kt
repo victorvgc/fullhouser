5dbde33a16151499793d79f6ef51dbbaab4667f2
victorvgc <victorvgc@hotmail.com>
2020-11-02 22:36:53 -0300

    Fixed when having tens "10s" on input

--- app/src/main/java/com/victorvgc/fullhouser/core/model/Card.kt
+++ app/src/main/java/com/victorvgc/fullhouser/core/model/Card.kt
@@ -13,19 +13,32 @@ class Card(
 
     companion object {
         fun fromString(s: String): Card {
-            val suit = if (s.length == 3) s[2].toString() else s[1].toString()
-            val value = if (s.length == 3) s.substring(0, 2) else s[0].toString()
+            val suit: String
+            val value: String
+
+            if (s.length == 2) {
+                suit = s[1].toString()
+                value = if (s[0].toString() == "0") "10" else s[0].toString()
+            } else {
+                suit = s[2].toString()
+                value = s.substring(0, 2)
+            }
 
             return Card(suit, value)
         }
     }
 
     @IgnoredOnParcel
-    val img: String =
+    val img: String = if (value != "10")
         "${value.toUpperCase(Locale.getDefault())}${suit.toUpperCase(Locale.getDefault())}.png"
+    else
+        "0${suit.toUpperCase(Locale.getDefault())}.png"
 
     override fun toString(): String {
-        return "$value$suit"
+        return if (value != "10")
+            "$value$suit"
+        else
+            "0$suit"
     }
 
     override fun equals(other: Any?): Boolean {
